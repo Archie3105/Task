05297d5fcaa92490472f75a6d6f82b6063667a8e
Eternal_Code <career.wipro0.com@gmail.com>
2024-04-13 23:32:39 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,62 @@
+const typed = new Typed(".type", {
+    strings: ["Web Sites", "Animation","Application"],
+    typeSpeed: 60,
+    backSpeed: 60,
+    loop: true,
+});
+
+
+   
+// document.body.style.backgroundColor="skyblue"
+
+
+/* ***************buttom-top button**************** */
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  
+    window.addEventListener("scroll", function () {
+        // Show or hide the button based on the scroll position
+
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    });
+  
+
+    scrollToTopBtn.addEventListener("click", function () {
+        // Scroll to the top of the page when the button is clicked
+       
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+        console.log("wirjubg")
+
+        document.body.scrollTop = 0; 
+        document.documentElement.scrollTop = 0; 
+    });
+  });
+
+
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     const chatButton = document.getElementById("showBtn");
+//     const chatBox = document.getElementsByClassName("chat");
+//     const closeButton = document.getElementById("closeBtn");
+
+//     chatButton.addEventListener("click", function() {
+//         chatBox.style.display = "block";
+//         console.log("done1")
+//     });
+
+//     closeButton.addEventListener("click", function() {
+//         chatBox.style.display = "none";
+//         console.log("done")
+//     });
+// });
+
